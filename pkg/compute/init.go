5a1b664b1c0f556408965181af9b74a18eadcf5b
Patrick Hamann <patrick@fastly.com>
2020-04-08 15:44:09 +0100

    Update init.go

--- pkg/compute/init.go
+++ pkg/compute/init.go
@@ -31,7 +31,7 @@ type template struct {
 
 const (
 	defaultTemplate       = "https://github.com/fastly/fastly-template-rust-default.git"
-	defaultTemplateBranch = "0.2.0-alpha3"
+	defaultTemplateBranch = "0.2.0-alpha4"
 	defaultTopLevelDomain = "edgecompute.app"
 	manageServiceBaseURL  = "https://manage.fastly.com/configure/services/"
 )
