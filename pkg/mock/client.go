5e3b6c3847343141c0026bcc1c0bd217511ec290
Colton McCurdy <cmccurdy@fastly.com>
2020-05-19 13:16:05 -0400

    mock: remove dependency on app package
    Signed-off-by: Colton McCurdy <cmccurdy@fastly.com>

--- pkg/mock/client.go
+++ pkg/mock/client.go
@@ -2,12 +2,11 @@ package mock
 
 import (
 	"github.com/fastly/cli/pkg/api"
-	"github.com/fastly/cli/pkg/app"
 )
 
 // APIClient takes a mock.API and returns an app.ClientFactory that uses that
 // mock, ignoring the token and endpoint. It should only be used for tests.
-func APIClient(a API) app.APIClientFactory {
+func APIClient(a API) func(string, string) (api.Interface, error) {
 	return func(token, endpoint string) (api.Interface, error) {
 		return a, nil
 	}
