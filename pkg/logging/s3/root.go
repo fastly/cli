e0b298788c6ee3a3c84986e4474344d38e8766c9
Colton J. McCurdy <cmccurdy@fastly.com>
2020-06-01 09:13:25 -0400

    logging: update s3 and bigquery root command docs to be consistent with other logging endpoints

--- pkg/logging/s3/root.go
+++ pkg/logging/s3/root.go
@@ -18,7 +18,7 @@ type RootCommand struct {
 func NewRootCommand(parent common.Registerer, globals *config.Data) *RootCommand {
 	var c RootCommand
 	c.Globals = globals
-	c.CmdClause = parent.Command("s3", "Manipulate Fastly service version S3 logging endpoints.")
+	c.CmdClause = parent.Command("s3", "Manipulate Fastly service version S3 logging endpoints")
 	return &c
 }
 
