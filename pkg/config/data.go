020d2ef917dbde3e5a3900c46b2cc1c38e1f3ed7
Mark McDonnell <integralist@fastly.com>
2021-01-20 15:49:01 +0000

    use proper abstraction level api for getting the base path

--- pkg/config/data.go
+++ pkg/config/data.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/fastly/cli/pkg/api"
@@ -154,7 +153,7 @@ func (f *File) Load(configEndpoint string, httpClient api.HTTPClient) error {
 	}
 
 	// Create the destination directory for the config file
-	basePath := strings.Split(FilePath, "config.toml")[0]
+	basePath := filepath.Dir(FilePath)
 	err = filesystem.MakeDirectoryIfNotExists(basePath)
 	if err != nil {
 		return err
