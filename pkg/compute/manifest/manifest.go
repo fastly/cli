b245fcfe92174486901b1a119a37c3353e686212
Mark McDonnell <Integralist@users.noreply.github.com>
2021-03-19 13:36:42 +0000

    Update pkg/compute/manifest/manifest.go
    Co-authored-by: Patrick Hamann <patrick@fastly.com>

--- pkg/compute/manifest/manifest.go
+++ pkg/compute/manifest/manifest.go
@@ -285,11 +285,7 @@ func stripManifestSection(r io.Reader, fpath string) (*bytes.Buffer, error) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		if scanner.Text() != "[manifest_version]" {
-			_, err := buf.Write(scanner.Bytes())
-			if err != nil {
-				return buf, err
-			}
-			_, err = buf.WriteString("\n")
+			_, err := buf.Write(append(scanner.Bytes(), []byte("\n"))))
 			if err != nil {
 				return buf, err
 			}
