2fadebf81500b210a12b47d8eff34ca3d3c7537f
Mark McDonnell <integralist@fastly.com>
2021-03-22 15:42:18 +0000

    Typo.

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -11,7 +11,7 @@ import (
 func Stale(lastVersionCheck string, dur string) bool {
 	d, err := time.ParseDuration("-" + dur)
 	if err != nil {
-		// if there' is no duration provided, then we should presume the loading of
+		// if there is no duration provided, then we should presume the loading of
 		// remote configuration failed and that we should retry that operation.
 		return true
 	}
