099ef099c43ed6c66012acd394a53a3e1ba75e71
Mark McDonnell <Integralist@users.noreply.github.com>
2021-04-13 11:11:07 +0100

    Remove flaky test logic. (#249)
    * Fix flaky test.
    
    * Remove flaky test logic.

--- pkg/compute/manifest/manifest_test.go
+++ pkg/compute/manifest/manifest_test.go
@@ -18,7 +18,6 @@ func TestManifest(t *testing.T) {
 		manifest      string
 		valid         bool
 		expectedError error
-		checkRef      bool
 	}{
 		"valid: semver": {
 			manifest: "fastly-valid-semver.toml",
@@ -31,7 +30,6 @@ func TestManifest(t *testing.T) {
 		"invalid: missing manifest_version causes default to be set": {
 			manifest: "fastly-invalid-missing-version.toml",
 			valid:    true,
-			checkRef: true,
 		},
 		"invalid: manifest_version as a section causes default to be set": {
 			manifest: "fastly-invalid-section-version.toml",
@@ -96,28 +94,6 @@ func TestManifest(t *testing.T) {
 				if err != nil {
 					t.Fatal(err)
 				}
-
-				// NOTE: we only assign checkRef to one of the invalid test cases, even
-				// though in practice the reference link will be added anytime a write
-				// operation is executed on the manifest file, because as far as the
-				// test suite is concerned it's constrained by the use of `once.Do()`
-				// at the package level of the manifest package.
-				//
-				// This is validating the write operation called when the
-				// manifest_version is invalid. We have a separate test function for
-				// validating the function that prepends the reference more explicitly.
-				if tc.checkRef {
-					b, err := os.ReadFile(path)
-					if err != nil {
-						t.Fatal(err)
-					}
-
-					content := string(b)
-
-					if !strings.Contains(content, SpecIntro) || !strings.Contains(content, SpecURL) {
-						t.Fatal("missing fastly.toml specification reference link")
-					}
-				}
 			} else {
 				// otherwise if we expect the manifest to be invalid/unrecognised then
 				// the error should match our expectations.
@@ -132,14 +108,14 @@ func TestManifest(t *testing.T) {
 func TestManifestPrepend(t *testing.T) {
 	prefix := filepath.Join("../", "testdata", "init")
 
-	// NOTE: the fixture file "fastly-invalid-missing-version.toml" will be
+	// NOTE: the fixture file "fastly-missing-spec-url.toml" will be
 	// overwritten by the test as the internal logic is supposed to add into the
 	// manifest a reference to the fastly.toml specification.
 	//
 	// To ensure future test runs complete successfully we do an initial read of
 	// the data and then write it back out when the tests have completed.
 
-	fpath := "fastly-invalid-missing-version.toml"
+	fpath := "fastly-missing-spec-url.toml"
 
 	path, err := filepath.Abs(filepath.Join(prefix, fpath))
 	if err != nil {
