87188ec4dbc21e8176dd4816af4e30a32fac6a84
Mark McDonnell <integralist@fastly.com>
2021-05-27 12:10:31 +0100

    new testutil package file for common test instructions

---
+++ pkg/testutil/api.go
@@ -0,0 +1,47 @@
+package testutil
+
+import (
+	"github.com/fastly/go-fastly/v3/fastly"
+)
+
+// ListVersionsOk returns a list of service versions in different states.
+//
+// The first element is active, the second is locked, the third is editable.
+func ListVersionsOk(i *fastly.ListVersionsInput) ([]*fastly.Version, error) {
+	return []*fastly.Version{
+		{
+			ServiceID: i.ServiceID,
+			Number:    1,
+			Active:    true,
+			UpdatedAt: MustParseTimeRFC3339("2000-01-01T01:00:00Z"),
+		},
+		{
+			ServiceID: i.ServiceID,
+			Number:    2,
+			Active:    false,
+			Locked:    true,
+			UpdatedAt: MustParseTimeRFC3339("2000-01-02T01:00:00Z"),
+		},
+		{
+			ServiceID: i.ServiceID,
+			Number:    3,
+			Active:    false,
+			UpdatedAt: MustParseTimeRFC3339("2000-01-02T01:00:00Z"),
+		},
+	}, nil
+}
+
+// GetActiveVersionOK returns an active service version (Number: 1).
+func GetActiveVersionOK(i *fastly.GetVersionInput) (*fastly.Version, error) {
+	return &fastly.Version{
+		ServiceID: i.ServiceID,
+		Number:    1,
+		Active:    true,
+		UpdatedAt: MustParseTimeRFC3339("2000-01-01T01:00:00Z"),
+	}, nil
+}
+
+// CloneVersionOK returns an incremented service version.
+func CloneVersionOK(i *fastly.CloneVersionInput) (*fastly.Version, error) {
+	return &fastly.Version{ServiceID: i.ServiceID, Number: i.ServiceVersion + 1}, nil
+}
