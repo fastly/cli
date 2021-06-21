d1286dab8b5d871b25c4d27e33c4e52d6562b790
Mark McDonnell <integralist@fastly.com>
2021-06-21 11:00:49 +0100

    clarify why we re-read config when checking for cli updates

--- pkg/update/check.go
+++ pkg/update/check.go
@@ -58,7 +58,9 @@ func CheckAsync(ctx context.Context, file config.File, configFilePath string, cu
 	return func(w io.Writer) {
 		result := <-results
 		if result.err == nil {
-			// `fastly configure` may have changed the file contents.
+			// If the user ran `fastly configure`, then the expectation is for the
+			// application configuration to have been updated. In that case we want
+			// to reread the config so we can update the LastChecked field.
 			if err := file.Read(configFilePath, in, out); err == nil {
 				file.CLI.LastChecked = time.Now().Format(time.RFC3339)
 				file.Write(configFilePath)
