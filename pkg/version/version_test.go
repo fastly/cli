2af5530e51cf263ebc7446958df98c56358e6a67
Mark McDonnell <integralist@fastly.com>
2021-06-30 17:58:57 +0100

    version to use existing abstraction

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -2,35 +2,24 @@ package version_test
 
 import (
 	"bytes"
-	"io"
 	"strings"
 	"testing"
 
-	"github.com/fastly/cli/pkg/api"
 	"github.com/fastly/cli/pkg/app"
-	"github.com/fastly/cli/pkg/config"
 	"github.com/fastly/cli/pkg/mock"
 	"github.com/fastly/cli/pkg/testutil"
-	"github.com/fastly/cli/pkg/update"
 )
 
 func TestVersion(t *testing.T) {
-	var (
-		args                            = []string{"version"}
-		env                             = config.Environment{}
-		file                            = config.File{}
-		configFileName                  = "/dev/null"
-		clientFactory                   = mock.APIClient(mock.API{})
-		httpClient     api.HTTPClient   = nil
-		cliVersioner   update.Versioner = mock.Versioner{Version: "v1.2.3"}
-		in             io.Reader        = nil
-		out            bytes.Buffer
-	)
-	err := app.Run(args, env, file, configFileName, clientFactory, httpClient, cliVersioner, in, &out)
+	var buf bytes.Buffer
+	args := testutil.Args("version")
+	api := mock.API{}
+	ara := testutil.NewAppRunArgs(args, api, &buf)
+	err := app.Run(ara.Args, ara.Env, ara.File, ara.AppConfigFile, ara.ClientFactory, ara.HTTPClient, ara.CLIVersioner, ara.In, ara.Out)
 	testutil.AssertNoError(t, err)
 	testutil.AssertString(t, strings.Join([]string{
 		"Fastly CLI version v0.0.0-unknown (unknown)",
 		"Built with go version unknown",
 		"",
-	}, "\n"), out.String())
+	}, "\n"), buf.String())
 }
