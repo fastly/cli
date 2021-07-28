c57e13d61c47e581d9f3d724114114cf6350a648
Mark McDonnell <integralist@fastly.com>
2021-07-28 12:20:59 +0100

    reorder placement of initialize function

--- pkg/commands/compute/language_javascript.go
+++ pkg/commands/compute/language_javascript.go
@@ -24,6 +24,54 @@ func NewJavaScript(timeout int) *JavaScript {
 	return &JavaScript{timeout}
 }
 
+// Initialize implements the Toolchain interface and initializes a newly cloned
+// package by installing required dependencies.
+func (a JavaScript) Initialize(out io.Writer) error {
+	// 1) Check `npm` is on $PATH
+	//
+	// npm is Node/JavaScript's toolchain package manager, it is needed to
+	// install the package dependencies on initialization. We only check whether
+	// the binary exists on the users $PATH and error with installation help text.
+	fmt.Fprintf(out, "Checking if npm is installed...\n")
+
+	p, err := exec.LookPath("npm")
+	if err != nil {
+		return errors.RemediationError{
+			Inner:       fmt.Errorf("`npm` not found in $PATH"),
+			Remediation: fmt.Sprintf("To fix this error, install Node.js and npm by visiting:\n\n\t$ %s", text.Bold("https://nodejs.org/")),
+		}
+	}
+
+	fmt.Fprintf(out, "Found npm at %s\n", p)
+
+	// 2) Check package.json file exists in $PWD
+	//
+	// A valid npm package manifest file is needed for the install command to
+	// work. Therefore, we first assert whether one exists in the current $PWD.
+	fpath, err := filepath.Abs("package.json")
+	if err != nil {
+		return fmt.Errorf("getting package.json path: %w", err)
+	}
+
+	if !filesystem.FileExists(fpath) {
+		return errors.RemediationError{
+			Inner:       fmt.Errorf("package.json not found"),
+			Remediation: fmt.Sprintf("To fix this error, run the following command:\n\n\t$ %s", text.Bold("npm init")),
+		}
+	}
+
+	fmt.Fprintf(out, "Found package.json at %s\n", fpath)
+	fmt.Fprintf(out, "Installing package dependencies...\n")
+
+	cmd := fstexec.Streaming{
+		Command: "npm",
+		Args:    []string{"install"},
+		Env:     []string{},
+		Output:  out,
+	}
+	return cmd.Exec()
+}
+
 // Verify implements the Toolchain interface and verifies whether the
 // JavaScript language toolchain is correctly configured on the host.
 func (a JavaScript) Verify(out io.Writer) error {
@@ -99,54 +147,6 @@ func (a JavaScript) Verify(out io.Writer) error {
 	fmt.Fprintf(out, "Found ecp-js at %s\n", path)
 
 	return nil
-}
-
-// Initialize implements the Toolchain interface and initializes a newly cloned
-// package by installing required dependencies.
-func (a JavaScript) Initialize(out io.Writer) error {
-	// 1) Check `npm` is on $PATH
-	//
-	// npm is Node/JavaScript's toolchain package manager, it is needed to
-	// install the package dependencies on initialization. We only check whether
-	// the binary exists on the users $PATH and error with installation help text.
-	fmt.Fprintf(out, "Checking if npm is installed...\n")
-
-	p, err := exec.LookPath("npm")
-	if err != nil {
-		return errors.RemediationError{
-			Inner:       fmt.Errorf("`npm` not found in $PATH"),
-			Remediation: fmt.Sprintf("To fix this error, install Node.js and npm by visiting:\n\n\t$ %s", text.Bold("https://nodejs.org/")),
-		}
-	}
-
-	fmt.Fprintf(out, "Found npm at %s\n", p)
-
-	// 2) Check package.json file exists in $PWD
-	//
-	// A valid npm package manifest file is needed for the install command to
-	// work. Therefore, we first assert whether one exists in the current $PWD.
-	fpath, err := filepath.Abs("package.json")
-	if err != nil {
-		return fmt.Errorf("getting package.json path: %w", err)
-	}
-
-	if !filesystem.FileExists(fpath) {
-		return errors.RemediationError{
-			Inner:       fmt.Errorf("package.json not found"),
-			Remediation: fmt.Sprintf("To fix this error, run the following command:\n\n\t$ %s", text.Bold("npm init")),
-		}
-	}
-
-	fmt.Fprintf(out, "Found package.json at %s\n", fpath)
-	fmt.Fprintf(out, "Installing package dependencies...\n")
-
-	cmd := fstexec.Streaming{
-		Command: "npm",
-		Args:    []string{"install"},
-		Env:     []string{},
-		Output:  out,
-	}
-	return cmd.Exec()
 }
 
 // Build implements the Toolchain interface and attempts to compile the package
