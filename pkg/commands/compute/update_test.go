44417e804b6a844782b25c21b742ac16b821e5fd
Mark McDonnell <integralist@fastly.com>
2021-08-03 14:43:16 +0100

    refactor TestUpdate

--- pkg/commands/compute/update_test.go
+++ pkg/commands/compute/update_test.go
@@ -13,6 +13,33 @@ import (
 )
 
 func TestUpdate(t *testing.T) {
+	// We're going to chdir to a deploy environment,
+	// so save the PWD to return to, afterwards.
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create test environment
+	rootdir := testutil.NewEnv(testutil.EnvOpts{
+		T: t,
+		Copy: []testutil.FileIO{
+			{
+				Src: filepath.Join("testdata", "deploy", "pkg", "package.tar.gz"),
+				Dst: filepath.Join("pkg", "package.tar.gz"),
+			},
+		},
+	})
+	defer os.RemoveAll(rootdir)
+
+	// Before running the test, chdir into the build environment.
+	// When we're done, chdir back to our original location.
+	// This is so we can reliably copy the testdata/ fixtures.
+	if err := os.Chdir(rootdir); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Chdir(pwd)
+
 	args := testutil.Args
 	scenarios := []testutil.TestScenario{
 		{
@@ -46,33 +73,6 @@ func TestUpdate(t *testing.T) {
 	}
 	for _, testcase := range scenarios {
 		t.Run(testcase.Name, func(t *testing.T) {
-			// We're going to chdir to a deploy environment,
-			// so save the PWD to return to, afterwards.
-			pwd, err := os.Getwd()
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			// Create test environment
-			rootdir := testutil.NewEnv(testutil.EnvOpts{
-				T: t,
-				Copy: []testutil.FileIO{
-					{
-						Src: filepath.Join("testdata", "deploy", "pkg", "package.tar.gz"),
-						Dst: filepath.Join("pkg", "package.tar.gz"),
-					},
-				},
-			})
-			defer os.RemoveAll(rootdir)
-
-			// Before running the test, chdir into the build environment.
-			// When we're done, chdir back to our original location.
-			// This is so we can reliably copy the testdata/ fixtures.
-			if err := os.Chdir(rootdir); err != nil {
-				t.Fatal(err)
-			}
-			defer os.Chdir(pwd)
-
 			var stdout bytes.Buffer
 			opts := testutil.NewRunOpts(testcase.Args, &stdout)
 			opts.APIClient = mock.APIClient(testcase.API)
