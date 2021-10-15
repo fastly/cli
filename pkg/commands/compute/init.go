c91ab694bc8d2d58eb85b74c87b8811e4a1ef2a9
Mark McDonnell <integralist@fastly.com>
2021-10-15 13:42:51 +0100

    refactor archive logic into separate package

--- pkg/commands/compute/init.go
+++ pkg/commands/compute/init.go
@@ -1,9 +1,6 @@
 package compute
 
 import (
-	"archive/tar"
-	"archive/zip"
-	"compress/gzip"
 	"crypto/rand"
 	"fmt"
 	"io"
@@ -22,6 +19,7 @@ import (
 	"github.com/fastly/cli/pkg/commands/compute/manifest"
 	"github.com/fastly/cli/pkg/config"
 	"github.com/fastly/cli/pkg/errors"
+	"github.com/fastly/cli/pkg/file"
 	"github.com/fastly/cli/pkg/filesystem"
 	"github.com/fastly/cli/pkg/text"
 )
@@ -214,23 +212,7 @@ func (c *InitCommand) Exec(in io.Reader, out io.Writer) (err error) {
 	}
 
 	if from != "" && !c.manifest.File.Exists() {
-		tar := &ArchiveGzip{
-			ArchiveBase{
-				Ext:   ".gz",
-				Mimes: []string{"application/gzip", "application/x-gzip"},
-			},
-		}
-
-		zip := &ArchiveZip{
-			ArchiveBase{
-				Ext:   ".zip",
-				Mimes: []string{"application/zip", "application/x-zip"},
-			},
-		}
-
-		var validArchives = []Archive{tar, zip}
-
-		err = pkgFetch(from, branch, tag, c.path, validArchives, progress, c.client, out, c.Globals.ErrLog)
+		err = pkgFetch(from, branch, tag, c.path, file.Archives, progress, c.client, out, c.Globals.ErrLog)
 		if err != nil {
 			c.Globals.ErrLog.AddWithContext(err, map[string]interface{}{
 				"From":   from,
@@ -541,212 +523,12 @@ func validateTemplateOptionOrURL(templates []config.StarterKit) func(string) err
 	}
 }
 
-// Archive represents the associated behaviour for a collection of files
-// contained inside an archive format.
-type Archive interface {
-	Extension() string
-	Extract() error
-	Filename() string
-	MimeTypes() []string
-	SetDestination(d string)
-	SetFile(r io.ReadSeeker)
-	SetFilename(n string)
-}
-
-// ArchiveBase represents a container for a collection of files.
-type ArchiveBase struct {
-	Dst   string
-	Ext   string
-	File  io.ReadSeeker
-	Mimes []string
-	Name  string
-}
-
-// Extension returns the file extension.
-func (a ArchiveBase) Extension() string {
-	return a.Ext
-}
-
-// MimeTypes returns all valid  mime types for the format.
-func (a ArchiveBase) MimeTypes() []string {
-	return a.Mimes
-}
-
-// Filename returns the file name.
-func (a ArchiveBase) Filename() string {
-	return a.Name
-}
-
-// SetDestination sets the destination for where files should be extracted.
-func (a *ArchiveBase) SetDestination(d string) {
-	a.Dst = d
-}
-
-// SetFile sets the local file descriptor where the archive should be written.
-//
-// NOTE: This archive file is the 'container' of the archived files that will
-// be extracted separately.
-func (a *ArchiveBase) SetFile(r io.ReadSeeker) {
-	a.File = r
-}
-
-// SetFilename sets the name of the local archive file.
-//
-// NOTE: This archive file is the 'container' of the archived files that will
-// be extracted separately.
-func (a *ArchiveBase) SetFilename(n string) {
-	a.Name = n
-}
-
-// ArchiveGzip represents a container for the .tar.gz file format.
-type ArchiveGzip struct {
-	ArchiveBase
-}
-
-// Extract all files and folders from the collection.
-func (a ArchiveGzip) Extract() error {
-	// NOTE: After the os.File has content written to it via io.Copy() inside
-	// pkgFetch(), we find the cursor index position is updated. This causes an
-	// EOF error when passing the file into gzip.NewReader() and so we need to
-	// first reset the cursor index.
-	_, err := a.File.Seek(0, io.SeekStart)
-	if err != nil {
-		return fmt.Errorf("failed to seek to the start of archive: %w", err)
-	}
-
-	gr, err := gzip.NewReader(a.File)
-	if err != nil {
-		return fmt.Errorf("error creating gzip reader: %w", err)
-	}
-	defer gr.Close()
-
-	tr := tar.NewReader(gr)
-
-	for {
-		header, err := tr.Next()
-
-		switch {
-
-		// If no more files are found return
-		case err == io.EOF:
-			return nil
-
-		// Return any other error
-		case err != nil:
-			return err
-
-		// If the header is nil, skip it
-		case header == nil:
-			continue
-
-		// Skip the any files duplicated as hidden files
-		case strings.HasPrefix(header.Name, "._"):
-			continue
-		}
-
-		// The target location where the dir/file should be created
-		segs := strings.Split(header.Name, string(filepath.Separator))
-		segs = segs[1:]
-		target := filepath.Join(a.Dst, filepath.Join(segs...))
-
-		fi := header.FileInfo()
-
-		if fi.IsDir() {
-			os.MkdirAll(target, os.ModePerm)
-			continue
-		}
-
-		if err = os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
-			return err
-		}
-
-		fd, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fi.Mode())
-		if err != nil {
-			return err
-		}
-
-		// NOTE: We use looped CopyN() not Copy() to avoid gosec G110 (CWE-409):
-		// Potential DoS vulnerability via decompression bomb.
-		for {
-			_, err := io.CopyN(fd, tr, 1024)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				return err
-			}
-		}
-
-		fd.Close()
-	}
-}
-
-// ArchiveZip represents a container for the .zip file format.
-type ArchiveZip struct {
-	ArchiveBase
-}
-
-// Extract all files and folders from the collection.
-func (a ArchiveZip) Extract() error {
-	r, err := zip.OpenReader(a.Name)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		// The zip contains a folder, and inside that folder are the files we're
-		// interested in. So while looping over the files (whose .Name field is the
-		// full path including the containing folder) we strip out the first path
-		// segment to ensure the files we need are extracted to the current directory.
-		segs := strings.Split(f.Name, string(filepath.Separator))
-		segs = segs[1:]
-		target := filepath.Join(a.Dst, filepath.Join(segs...))
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(target, os.ModePerm)
-			continue
-		}
-
-		if err = os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
-			return err
-		}
-
-		fd, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			return err
-		}
-
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-
-		// NOTE: We use looped CopyN() not Copy() to avoid gosec G110 (CWE-409):
-		// Potential DoS vulnerability via decompression bomb.
-		for {
-			_, err := io.CopyN(fd, rc, 1024)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				return err
-			}
-		}
-
-		fd.Close()
-		rc.Close()
-	}
-
-	return nil
-}
-
 // pkgFetch will determine if the package code should be fetched from GitHub
 // using the git binary to clone the source or a HTTP request that uses
 // content-negotiation to determine the type of archive format used.
 func pkgFetch(
 	from, branch, tag, dst string,
-	validArchives []Archive,
+	archives []file.Archive,
 	progress text.Progress,
 	client api.HTTPClient,
 	out io.Writer,
@@ -766,7 +548,7 @@ func pkgFetch(
 		return fmt.Errorf("failed to construct package request URL: %w", err)
 	}
 
-	for _, archive := range validArchives {
+	for _, archive := range archives {
 		for _, mime := range archive.MimeTypes() {
 			req.Header.Add("Accept", mime)
 		}
@@ -810,11 +592,11 @@ func pkgFetch(
 		return fmt.Errorf("failed to write %s archive to disk: %w", filename, err)
 	}
 
-	var archive Archive
+	var archive file.Archive
 
 mimes:
 	for _, mimetype := range res.Header.Values("Content-Type") {
-		for _, a := range validArchives {
+		for _, a := range archives {
 			for _, mime := range a.MimeTypes() {
 				if mimetype == mime {
 					archive = a
@@ -827,7 +609,7 @@ mimes:
 	if archive == nil {
 		ext := filepath.Ext(filename)
 
-		for _, a := range validArchives {
+		for _, a := range archives {
 			if ext == a.Extension() {
 				archive = a
 				break
---
+++ pkg/file/archive.go
@@ -0,0 +1,231 @@
+package file
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// TarGz represents an instance of a tar.gz archive file.
+var TarGz = &ArchiveGzip{
+	ArchiveBase{
+		Ext:   ".gz",
+		Mimes: []string{"application/gzip", "application/x-gzip"},
+	},
+}
+
+// Zip represents an instance of a zip archive file.
+var Zip = &ArchiveZip{
+	ArchiveBase{
+		Ext:   ".zip",
+		Mimes: []string{"application/zip", "application/x-zip"},
+	},
+}
+
+// Archives is a collection of supported archive formats.
+var Archives = []Archive{TarGz, Zip}
+
+// Archive represents the associated behaviour for a collection of files
+// contained inside an archive format.
+type Archive interface {
+	Extension() string
+	Extract() error
+	Filename() string
+	MimeTypes() []string
+	SetDestination(d string)
+	SetFile(r io.ReadSeeker)
+	SetFilename(n string)
+}
+
+// ArchiveBase represents a container for a collection of files.
+type ArchiveBase struct {
+	Dst   string
+	Ext   string
+	File  io.ReadSeeker
+	Mimes []string
+	Name  string
+}
+
+// Extension returns the file extension.
+func (a ArchiveBase) Extension() string {
+	return a.Ext
+}
+
+// MimeTypes returns all valid  mime types for the format.
+func (a ArchiveBase) MimeTypes() []string {
+	return a.Mimes
+}
+
+// Filename returns the file name.
+func (a ArchiveBase) Filename() string {
+	return a.Name
+}
+
+// SetDestination sets the destination for where files should be extracted.
+func (a *ArchiveBase) SetDestination(d string) {
+	a.Dst = d
+}
+
+// SetFile sets the local file descriptor where the archive should be written.
+//
+// NOTE: This archive file is the 'container' of the archived files that will
+// be extracted separately.
+func (a *ArchiveBase) SetFile(r io.ReadSeeker) {
+	a.File = r
+}
+
+// SetFilename sets the name of the local archive file.
+//
+// NOTE: This archive file is the 'container' of the archived files that will
+// be extracted separately.
+func (a *ArchiveBase) SetFilename(n string) {
+	a.Name = n
+}
+
+// ArchiveGzip represents a container for the .tar.gz file format.
+type ArchiveGzip struct {
+	ArchiveBase
+}
+
+// Extract all files and folders from the collection.
+func (a ArchiveGzip) Extract() error {
+	// NOTE: After the os.File has content written to it via io.Copy() inside
+	// pkgFetch(), we find the cursor index position is updated. This causes an
+	// EOF error when passing the file into gzip.NewReader() and so we need to
+	// first reset the cursor index.
+	_, err := a.File.Seek(0, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("failed to seek to the start of archive: %w", err)
+	}
+
+	gr, err := gzip.NewReader(a.File)
+	if err != nil {
+		return fmt.Errorf("error creating gzip reader: %w", err)
+	}
+	defer gr.Close()
+
+	tr := tar.NewReader(gr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// If no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// Return any other error
+		case err != nil:
+			return err
+
+		// If the header is nil, skip it
+		case header == nil:
+			continue
+
+		// Skip the any files duplicated as hidden files
+		case strings.HasPrefix(header.Name, "._"):
+			continue
+		}
+
+		// The target location where the dir/file should be created
+		segs := strings.Split(header.Name, string(filepath.Separator))
+		segs = segs[1:]
+		target := filepath.Join(a.Dst, filepath.Join(segs...))
+
+		fi := header.FileInfo()
+
+		if fi.IsDir() {
+			os.MkdirAll(target, os.ModePerm)
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
+			return err
+		}
+
+		fd, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fi.Mode())
+		if err != nil {
+			return err
+		}
+
+		// NOTE: We use looped CopyN() not Copy() to avoid gosec G110 (CWE-409):
+		// Potential DoS vulnerability via decompression bomb.
+		for {
+			_, err := io.CopyN(fd, tr, 1024)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+		}
+
+		fd.Close()
+	}
+}
+
+// ArchiveZip represents a container for the .zip file format.
+type ArchiveZip struct {
+	ArchiveBase
+}
+
+// Extract all files and folders from the collection.
+func (a ArchiveZip) Extract() error {
+	r, err := zip.OpenReader(a.Name)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		// The zip contains a folder, and inside that folder are the files we're
+		// interested in. So while looping over the files (whose .Name field is the
+		// full path including the containing folder) we strip out the first path
+		// segment to ensure the files we need are extracted to the current directory.
+		segs := strings.Split(f.Name, string(filepath.Separator))
+		segs = segs[1:]
+		target := filepath.Join(a.Dst, filepath.Join(segs...))
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(target, os.ModePerm)
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
+			return err
+		}
+
+		fd, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		// NOTE: We use looped CopyN() not Copy() to avoid gosec G110 (CWE-409):
+		// Potential DoS vulnerability via decompression bomb.
+		for {
+			_, err := io.CopyN(fd, rc, 1024)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+		}
+
+		fd.Close()
+		rc.Close()
+	}
+
+	return nil
+}
