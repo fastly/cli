0ab7b85f5d518024ed0101acc861eb0cfb634c56
Mark McDonnell <integralist@fastly.com>
2021-12-20 10:43:46 +0000

    add common flags files

---
+++ pkg/cmd/common.go
@@ -0,0 +1,8 @@
+package cmd
+
+var (
+	FlagVersionName    = "version"
+	FlagVersionDesc    = "'latest', 'active', or the number of a specific version"
+	FlagCustomerIDName = "customer-id"
+	FlagCustomerIDDesc = "Alphanumeric string identifying the customer (falls back to FASTLY_CUSTOMER_ID)"
+)
