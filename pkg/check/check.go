56e8f8f1ae7a6cfa0e152ab996639b7f6b34f7da
Integralist <integralist@fastly.com>
2022-05-12 17:47:16 +0100

    simplify the stale logic

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -9,16 +9,13 @@ import (
 // EXAMPLE:
 // dur is a string like "24h", "10m" or "5s".
 func Stale(lastVersionCheck string, dur string) bool {
-	d, err := time.ParseDuration("-" + dur)
+	ttl, err := time.ParseDuration(dur)
 	if err != nil {
 		// If there is no duration provided, then we should presume the loading of
 		// remote configuration failed and that we should retry that operation.
 		return true
 	}
 
-	if t, _ := time.Parse(time.RFC3339, lastVersionCheck); !t.Before(time.Now().Add(d)) {
-		return false
-	}
-
-	return true
+	lastChecked, _ := time.Parse(time.RFC3339, lastVersionCheck)
+	return lastChecked.Add(ttl).Before(time.Now())
 }
