afc7593479f000135b2bbdf3d96bf792bcb3c47b
Integralist <integralist@fastly.com>
2022-10-20 20:21:39 +0100

    fix ci

--- scripts/config.sh
+++ scripts/config.sh
@@ -19,7 +19,12 @@ kits=(
 # The last line of the template file is still used by an internal DevHub process
 # but should be removed for the CLI's CI process.
 cp ./.fastly/config.toml ./pkg/config/config.toml
-sed -i '' -e '$ d' ./pkg/config/config.toml
+
+if [[ $(uname) == "Darwin" ]]; then
+  sed -i '' -e '$ d' ./pkg/config/config.toml
+else
+  sed -i '$ d' ./pkg/config/config.toml
+fi
 
 function parse() {
   tomlq -f "./$k.toml" $1
