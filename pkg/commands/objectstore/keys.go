0dcf6b3723201ec5125817c3830b01998c0f837a
Mark McDonnell <Integralist@users.noreply.github.com>
2023-01-17 10:08:42 +0000

    feat(objectstore): add `--json` support to keys/list subcommands (#762)
    * fix(objectstore/keys): fix flag description (suggests wrong input)
    
    * breaking(objectstore/keys): change flags to have long/short variants
    
    * feat(objectstore/keys): implement --json flag
    
    * feat(objectstore/list): implement --json flag
    
    * fix(text/objectstore): add spacing to output

--- pkg/commands/objectstore/keys.go
+++ pkg/commands/objectstore/keys.go
@@ -1,10 +1,13 @@
 package objectstore
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/fastly/cli/pkg/cmd"
 	"github.com/fastly/cli/pkg/config"
+	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/manifest"
 	"github.com/fastly/cli/pkg/text"
 	"github.com/fastly/go-fastly/v7/fastly"
@@ -13,6 +16,7 @@ import (
 // KeysCommand calls the Fastly API to list the keys for a given object store.
 type KeysCommand struct {
 	cmd.Base
+	json     bool
 	manifest manifest.Data
 	Input    fastly.ListObjectStoreKeysInput
 }
@@ -23,16 +27,43 @@ func NewKeysCommand(parent cmd.Registerer, globals *config.Data, data manifest.D
 	c.Globals = globals
 	c.manifest = data
 	c.CmdClause = parent.Command("keys", "List Fastly object store keys")
+
+	// required
 	c.CmdClause.Flag("id", "ID of object store").Required().StringVar(&c.Input.ID)
+
+	// optional
+	c.RegisterFlagBool(cmd.BoolFlagOpts{
+		Name:        cmd.FlagJSONName,
+		Description: cmd.FlagJSONDesc,
+		Dst:         &c.json,
+		Short:       'j',
+	})
 	return &c
 }
 
 // Exec invokes the application logic for the command.
 func (c *KeysCommand) Exec(_ io.Reader, out io.Writer) error {
+	if c.Globals.Verbose() && c.json {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
 	o, err := c.Globals.APIClient.ListObjectStoreKeys(&c.Input)
 	if err != nil {
 		c.Globals.ErrLog.Add(err)
 		return err
+	}
+
+	if c.json {
+		data, err := json.Marshal(o)
+		if err != nil {
+			return err
+		}
+		_, err = out.Write(data)
+		if err != nil {
+			c.Globals.ErrLog.Add(err)
+			return fmt.Errorf("error: unable to write data to stdout: %w", err)
+		}
+		return nil
 	}
 
 	text.PrintObjectStoreKeys(out, "", o.Data)
--- pkg/commands/objectstore/list.go
+++ pkg/commands/objectstore/list.go
@@ -1,10 +1,13 @@
 package objectstore
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/fastly/cli/pkg/cmd"
 	"github.com/fastly/cli/pkg/config"
+	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/manifest"
 	"github.com/fastly/cli/pkg/text"
 	"github.com/fastly/go-fastly/v7/fastly"
@@ -13,6 +16,7 @@ import (
 // ListCommand calls the Fastly API to list the available object stores.
 type ListCommand struct {
 	cmd.Base
+	json     bool
 	manifest manifest.Data
 	Input    fastly.ListObjectStoresInput
 }
@@ -26,15 +30,41 @@ func NewListCommand(parent cmd.Registerer, globals *config.Data, data manifest.D
 		manifest: data,
 	}
 	c.CmdClause = parent.Command("list", "List Fastly object stores")
+
+	// optional
+	c.RegisterFlagBool(cmd.BoolFlagOpts{
+		Name:        cmd.FlagJSONName,
+		Description: cmd.FlagJSONDesc,
+		Dst:         &c.json,
+		Short:       'j',
+	})
+
 	return &c
 }
 
 // Exec invokes the application logic for the command.
 func (c *ListCommand) Exec(_ io.Reader, out io.Writer) error {
+	if c.Globals.Verbose() && c.json {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
 	o, err := c.Globals.APIClient.ListObjectStores(&c.Input)
 	if err != nil {
 		c.Globals.ErrLog.Add(err)
 		return err
+	}
+
+	if c.json {
+		data, err := json.Marshal(o)
+		if err != nil {
+			return err
+		}
+		_, err = out.Write(data)
+		if err != nil {
+			c.Globals.ErrLog.Add(err)
+			return fmt.Errorf("error: unable to write data to stdout: %w", err)
+		}
+		return nil
 	}
 
 	for _, o := range o.Data {
