a522f103b253ebb043c33904fa53acf85ee832a1
Federico G. Schwindt <fgsch@lodoss.net>
2023-02-06 15:57:22 +0000

    Add json output support to object-store create

--- pkg/commands/objectstore/create.go
+++ pkg/commands/objectstore/create.go
@@ -1,10 +1,13 @@
 package objectstore
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/fastly/cli/pkg/cmd"
 	"github.com/fastly/cli/pkg/config"
+	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/manifest"
 	"github.com/fastly/cli/pkg/text"
 	"github.com/fastly/go-fastly/v7/fastly"
@@ -13,6 +16,7 @@ import (
 // CreateCommand calls the Fastly API to create an object store.
 type CreateCommand struct {
 	cmd.Base
+	json     bool
 	manifest manifest.Data
 	Input    fastly.CreateObjectStoreInput
 }
@@ -27,17 +31,40 @@ func NewCreateCommand(parent cmd.Registerer, globals *config.Data, data manifest
 	}
 	c.CmdClause = parent.Command("create", "Create an object store")
 	c.CmdClause.Flag("name", "Name of Object Store").Short('n').Required().StringVar(&c.Input.Name)
+	c.RegisterFlagBool(cmd.BoolFlagOpts{
+		Name:        cmd.FlagJSONName,
+		Description: cmd.FlagJSONDesc,
+		Dst:         &c.json,
+		Short:       'j',
+	})
 	return &c
 }
 
 // Exec invokes the application logic for the command.
 func (c *CreateCommand) Exec(_ io.Reader, out io.Writer) error {
-	d, err := c.Globals.APIClient.CreateObjectStore(&c.Input)
+	if c.Globals.Verbose() && c.json {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
+	o, err := c.Globals.APIClient.CreateObjectStore(&c.Input)
 	if err != nil {
 		c.Globals.ErrLog.Add(err)
 		return err
 	}
 
-	text.Success(out, "Created object store %s (name %s)", d.ID, d.Name)
+	if c.json {
+		data, err := json.Marshal(o)
+		if err != nil {
+			return err
+		}
+		_, err = out.Write(data)
+		if err != nil {
+			c.Globals.ErrLog.Add(err)
+			return fmt.Errorf("error: unable to write data to stdout: %w", err)
+		}
+		return nil
+	}
+
+	text.Success(out, "Created object store %s (name %s)", o.ID, o.Name)
 	return nil
 }
