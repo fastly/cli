8814d8de28552df0abce12488c16b19586111e33
Integralist <integralist@fastly.com>
2023-04-17 16:35:26 +0100

    feat(service): support --json for `service search` subcommand

--- pkg/commands/service/search.go
+++ pkg/commands/service/search.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/fastly/cli/pkg/cmd"
+	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/global"
 	"github.com/fastly/cli/pkg/manifest"
 	"github.com/fastly/cli/pkg/text"
@@ -13,8 +14,10 @@ import (
 // SearchCommand calls the Fastly API to describe a service.
 type SearchCommand struct {
 	cmd.Base
-	manifest manifest.Data
+	cmd.JSONOutput
+
 	Input    fastly.SearchServiceInput
+	manifest manifest.Data
 }
 
 // NewSearchCommand returns a usable command registered under the parent.
@@ -26,19 +29,31 @@ func NewSearchCommand(parent cmd.Registerer, g *global.Data, m manifest.Data) *S
 		manifest: m,
 	}
 	c.CmdClause = parent.Command("search", "Search for a Fastly service by name")
-	// required
+
+	// Required.
 	c.CmdClause.Flag("name", "Service name").Short('n').Required().StringVar(&c.Input.Name)
+
+	// Optional.
+	c.RegisterFlagBool(c.JSONFlag()) // --json
 
 	return &c
 }
 
 // Exec invokes the application logic for the command.
 func (c *SearchCommand) Exec(_ io.Reader, out io.Writer) error {
+	if c.Globals.Verbose() && c.JSONOutput.Enabled {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
 	service, err := c.Globals.APIClient.SearchService(&c.Input)
 	if err != nil {
 		c.Globals.ErrLog.AddWithContext(err, map[string]any{
 			"Service Name": c.Input.Name,
 		})
+		return err
+	}
+
+	if ok, err := c.WriteJSON(out, service); ok {
 		return err
 	}
 
