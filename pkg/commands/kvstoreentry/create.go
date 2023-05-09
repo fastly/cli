1e1fd1aed861a54574ce3f75894fd92a069f1881
Integralist <integralist@fastly.com>
2023-05-09 12:34:27 +0100

    refactor(kvstore/create): switch to concurrent calls for each key insertion

--- pkg/commands/kvstoreentry/create.go
+++ pkg/commands/kvstoreentry/create.go
@@ -2,10 +2,9 @@ package kvstoreentry
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/base64"
 	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -33,7 +32,7 @@ func NewCreateCommand(parent cmd.Registerer, g *global.Data, m manifest.Data) *C
 	}
 	c.CmdClause = parent.Command("create", "Insert a key-value pair").Alias("insert")
 	c.CmdClause.Flag("dir", "Path to a directory containing individual files where the filename is the key and the file contents is the value").StringVar(&c.dirPath)
-	c.CmdClause.Flag("dir-batch-size", "Files will be processed in batches").Default("10").IntVar(&c.dirBatchSize)
+	c.CmdClause.Flag("dir-concurrency", "Limit the number of concurrent network resources allocated").Default("100").IntVar(&c.dirConcurrency)
 	c.CmdClause.Flag("file", "Path to a file containing individual JSON objects separated by new-line delimiter").StringVar(&c.filePath)
 	c.CmdClause.Flag("key-name", "Key name").Short('k').StringVar(&c.Input.Key)
 	c.CmdClause.Flag("stdin", "Read new-line separated JSON stream via STDIN").BoolVar(&c.stdin)
@@ -45,11 +44,11 @@ func NewCreateCommand(parent cmd.Registerer, g *global.Data, m manifest.Data) *C
 // CreateCommand calls the Fastly API to insert a key into an kv store.
 type CreateCommand struct {
 	cmd.Base
-	dirBatchSize int
-	dirPath      string
-	filePath     string
-	manifest     manifest.Data
-	stdin        bool
+	dirConcurrency int
+	dirPath        string
+	filePath       string
+	manifest       manifest.Data
+	stdin          bool
 
 	Input fastly.InsertKVStoreKeyInput
 }
@@ -111,7 +110,7 @@ func (c *CreateCommand) ProcessStdin(in io.Reader, out io.Writer) error {
 		return fsterr.ErrNoSTDINData
 	}
 
-	if err := c.CallEndpoint(in); err != nil {
+	if err := c.CallBatchEndpoint(in); err != nil {
 		c.Globals.ErrLog.Add(err)
 		return err
 	}
@@ -127,7 +126,7 @@ func (c *CreateCommand) ProcessFile(_ io.Reader, out io.Writer) error {
 		return err
 	}
 
-	if err := c.CallEndpoint(f); err != nil {
+	if err := c.CallBatchEndpoint(f); err != nil {
 		c.Globals.ErrLog.Add(err)
 		return err
 	}
@@ -156,69 +155,74 @@ func (c *CreateCommand) ProcessDir(_ io.Reader, out io.Writer) error {
 	if err != nil {
 		return err
 	}
-	processing := fmt.Sprintf("Processing %d files", len(files))
-	msg := "%s (files read: %d, api calls made: %d)"
-	spinner.Message(fmt.Sprintf(msg, processing, 0, 0) + "...")
+	fileLength := len(files)
+	msg := "Processed %d of %d files"
+	spinner.Message(fmt.Sprintf(msg, 0, fileLength) + "...")
 
 	base := filepath.Base(path)
 	errors := []ProcessErr{}
-	errMessages := make(chan ProcessErr)
-	fileRead := make(chan bool)
-	apiCall := make(chan bool)
+	processed := make(chan struct{}, c.dirConcurrency)
+	sem := make(chan struct{}, c.dirConcurrency)
 
 	var (
-		apiCalls  int
-		filesRead int
-		fileBatch []string
-		wg        sync.WaitGroup
+		filesProcessed uint64
+		wg             sync.WaitGroup
 	)
 
 	go func() {
-		for msg := range errMessages {
-			fmt.Println("received message to errMessages channel")
-			errors = append(errors, msg)
-		}
-	}()
-
-	go func() {
-		for ok := range fileRead {
-			if ok {
-				fmt.Println("received message to fileRead channel")
-				filesRead++
-				spinner.Message(fmt.Sprintf(msg, processing, filesRead, apiCalls) + "...")
-			}
-		}
-	}()
-
-	go func() {
-		for ok := range apiCall {
-			if ok {
-				fmt.Println("received message to apiCall channel")
-				apiCalls++
-				spinner.Message(fmt.Sprintf(msg, processing, filesRead, apiCalls) + "...")
-			}
+		for range processed {
+			filesProcessed++
+			spinner.Message(fmt.Sprintf(msg, filesProcessed, fileLength) + "...")
 		}
 	}()
 
 	for _, file := range files {
 		if !file.IsDir() {
-			fileBatch = append(fileBatch, filepath.Join(c.dirPath, file.Name()))
-			if len(fileBatch) == c.dirBatchSize {
-				wg.Add(1)
-				go processBatch(base, fileBatch, &wg, c.CallEndpoint, &errMessages, &fileRead, &apiCall)
-				fileBatch = nil
-			}
-		}
-	}
+			wg.Add(1)
 
-	if len(fileBatch) > 0 {
-		wg.Add(1)
-		go processBatch(base, fileBatch, &wg, c.CallEndpoint, &errMessages, &fileRead, &apiCall)
+			go func(file fs.DirEntry) {
+				// Restrict resource allocation if concurrency limit is exceeded.
+				sem <- struct{}{}
+				defer func() {
+					<-sem
+				}()
+				defer wg.Done()
+
+				filePath := filepath.Join(c.dirPath, file.Name())
+				dir, filename := filepath.Split(filePath)
+				index := strings.Index(dir, base)
+				filename = filepath.Join(dir[index:], filename)
+
+				fileContent, err := os.ReadFile(filePath)
+				if err != nil {
+					errors = append(errors, ProcessErr{
+						File: filePath,
+						Err:  err,
+					})
+					return
+				}
+
+				err = c.Globals.APIClient.InsertKVStoreKey(&fastly.InsertKVStoreKeyInput{
+					ID:    c.Input.ID,
+					Key:   filename,
+					Value: string(fileContent),
+				})
+				if err != nil {
+					errors = append(errors, ProcessErr{
+						File: filePath,
+						Err:  err,
+					})
+					return
+				}
+
+				processed <- struct{}{}
+			}(file)
+		}
 	}
 
 	wg.Wait()
 
-	spinner.StopMessage(fmt.Sprintf(msg, processing, filesRead, apiCalls))
+	spinner.StopMessage(fmt.Sprintf(msg, filesProcessed, fileLength))
 	err = spinner.Stop()
 	if err != nil {
 		return err
@@ -229,10 +233,7 @@ func (c *CreateCommand) ProcessDir(_ io.Reader, out io.Writer) error {
 		return nil
 	}
 
-	// NOTE: We can't be accurate because a batch of files might fail.
-	// The API response doesn't indicate what files from the batch failed.
-	// Just that the batch failed to upload.
-	text.Error(out, "Inserted (approx) %d keys into KV Store", len(files)-len(errors))
+	text.Error(out, "Inserted %d keys into KV Store", len(files)-len(errors))
 
 	for _, err := range errors {
 		fmt.Printf("File: %s\nError: %s\n\n", err.File, err.Err.Error())
@@ -241,8 +242,7 @@ func (c *CreateCommand) ProcessDir(_ io.Reader, out io.Writer) error {
 	return nil
 }
 
-func (c *CreateCommand) CallEndpoint(in io.Reader) error {
-	fmt.Printf("%+v\n", "api call started")
+func (c *CreateCommand) CallBatchEndpoint(in io.Reader) error {
 	host, _ := c.Globals.Endpoint()
 	path := fmt.Sprintf("/resources/stores/kv/%s/batch", c.Input.ID)
 	token, s := c.Globals.Token()
@@ -269,58 +269,7 @@ func (c *CreateCommand) CallEndpoint(in io.Reader) error {
 		return fmt.Errorf("%w: %d %s: %s", err, apiErr.StatusCode, http.StatusText(apiErr.StatusCode), string(resp))
 	}
 
-	fmt.Printf("%+v\n", "api call finished")
 	return nil
-}
-
-func processBatch(
-	base string,
-	filePaths []string,
-	wg *sync.WaitGroup,
-	callEndpoint func(in io.Reader) error,
-	errMessages *chan ProcessErr,
-	fileRead *chan bool,
-	apiCall *chan bool,
-) {
-	defer wg.Done()
-
-	var payload bytes.Buffer
-	template := `{"key": "%s", "value": "%s"}`
-
-	for _, filePath := range filePaths {
-		// gosec flagged this:
-		// G304 (CWE-22): Potential file inclusion via variable
-		// #nosec
-		fileContent, err := os.ReadFile(filePath)
-		if err != nil {
-			fmt.Println("send to errMessages channel (file read error)")
-			*errMessages <- ProcessErr{
-				File: filePath,
-				Err:  err,
-			}
-			continue
-		}
-
-		dir, filename := filepath.Split(filePath)
-		index := strings.Index(dir, base)
-		filename = filepath.Join(dir[index:], filename)
-
-		_, _ = payload.WriteString(fmt.Sprintf(template, filename, base64.StdEncoding.EncodeToString(fileContent)))
-		_ = payload.WriteByte('\n')
-		fmt.Println("send to fileRead channel")
-		*fileRead <- true
-	}
-
-	err := callEndpoint(&payload)
-	fmt.Println("send to apiCall channel")
-	*apiCall <- true
-	if err != nil {
-		fmt.Println("send to errMessages channel (api call error)")
-		*errMessages <- ProcessErr{
-			File: "Batch",
-			Err:  err,
-		}
-	}
 }
 
 type ProcessErr struct {
