22bff079ad7f39038521950940fbb0136009ce93
Integralist <integralist@fastly.com>
2023-05-15 15:58:22 +0100

    Revert "fix(kvstoreentry): implement Windows check for hidden file attribute"
    This reverts commit 24422570c89750988321949bfb8815bb62417083.

--- pkg/commands/kvstoreentry/hidden.go
+++ pkg/commands/kvstoreentry/hidden.go
@@ -1,8 +1,5 @@
-//go:build !windows
-
 package kvstoreentry
 
-// isHiddenFile checks if a file or directory is a hidden dotfile
 func isHiddenFile(filename string) (bool, error) {
 	return filename[0] == '.', nil
 }
