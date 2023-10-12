bb47549dccb15a2b92f898d7d1c078869cd7ebc5
Integralist <integralist@fastly.com>
2023-10-12 11:18:50 +0100

    fix(compute/deploy): add DeleteService call to test

--- pkg/commands/compute/deploy_test.go
+++ pkg/commands/compute/deploy_test.go
@@ -1708,7 +1708,7 @@ func TestDeploy(t *testing.T) {
 			},
 		},
 		{
-			name: "success with setup.kv_stores configuration and no existing service with file and value on same key",
+			name: "error with setup.kv_stores configuration and no existing service with file and value on same key",
 			args: args("compute deploy --token 123"),
 			api: mock.API{
 				CreateBackendFn:     createBackendOK,
@@ -1722,6 +1722,7 @@ func TestDeploy(t *testing.T) {
 				GetServiceDetailsFn: getServiceDetailsWasm,
 				ListDomainsFn:       listDomainsOk,
 				ListVersionsFn:      testutil.ListVersions,
+				DeleteServiceFn:     deleteServiceOK,
 			},
 			httpClientRes: []*http.Response{
 				{
