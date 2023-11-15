7afc76e5d8a131a94669fa9e9e1b33440ca49884
Mark McDonnell <Integralist@users.noreply.github.com>
2023-11-15 17:04:32 +0000

    fix(errors): ensure help output is displayed (#1092)

--- cmd/fastly/main.go
+++ cmd/fastly/main.go
@@ -2,10 +2,7 @@
 package main
 
 import (
-	"errors"
 	"os"
-
-	"github.com/fatih/color"
 
 	"github.com/fastly/cli/pkg/app"
 	fsterr "github.com/fastly/cli/pkg/errors"
@@ -13,28 +10,9 @@ import (
 
 func main() {
 	if err := app.Run(os.Args, os.Stdin); err != nil {
-		if skipExit := processErr(err, os.Args); skipExit {
+		if skipExit := fsterr.Process(err, os.Args, os.Stdout); skipExit {
 			return
 		}
 		os.Exit(1)
 	}
-}
-
-// processErr persists the error log to disk and deduces the error type.
-func processErr(err error, args []string) (skipExit bool) {
-	// NOTE: We persist any error log entries to disk before attempting to handle
-	// a possible error response from app.Run as there could be errors recorded
-	// during the execution flow but were otherwise handled without bubbling an
-	// error back the call stack, and so if the user still experiences something
-	// unexpected we will have a record of any errors that happened along the way.
-	logErr := fsterr.Log.Persist(fsterr.LogPath, args[1:])
-	if logErr != nil {
-		fsterr.Deduce(logErr).Print(color.Error)
-	}
-	exitError := fsterr.SkipExitError{}
-	if errors.As(err, &exitError) {
-		return exitError.Skip
-	}
-	fsterr.Deduce(err).Print(color.Error)
-	return false
 }
