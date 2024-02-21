b9e266cd5714b1d9a55ce1016aa7772da3910247
Integralist <integralist@fastly.com>
2024-02-21 10:56:18 +0000

    style(mock/client): fix linter issues

--- pkg/mock/client.go
+++ pkg/mock/client.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/fastly/go-fastly/v9/fastly"
@@ -12,6 +13,9 @@ import (
 // mock, ignoring the token and endpoint. It should only be used for tests.
 func APIClient(a API) func(string, string, bool) (api.Interface, error) {
 	return func(token, endpoint string, debugMode bool) (api.Interface, error) {
+		fmt.Printf("token: %s\n", token)
+		fmt.Printf("endpoint: %s\n", endpoint)
+		fmt.Printf("debugMode: %t\n", debugMode)
 		return a, nil
 	}
 }
@@ -26,14 +30,18 @@ type HTTPClient struct {
 	Errors []error
 }
 
+// Get mocks a HTTP Client Get request.
 func (c HTTPClient) Get(p string, _ *fastly.RequestOptions) (*http.Response, error) {
+	fmt.Printf("p: %#v\n", p)
 	// IMPORTANT: Have to increment on defer as index is already 0 by this point.
 	// This is opposite to the Do() method which is -1 at the time it's called.
 	defer func() { c.Index++ }()
 	return c.Responses[c.Index], c.Errors[c.Index]
 }
 
+// Do mocks a HTTP Client Do operation.
 func (c HTTPClient) Do(r *http.Request) (*http.Response, error) {
+	fmt.Printf("r: %#v\n", r)
 	c.Index++
 	return c.Responses[c.Index], c.Errors[c.Index]
 }
