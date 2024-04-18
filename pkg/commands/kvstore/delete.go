fbd379427a07667ff2aeada1a8e28921f600ea80
Mark McDonnell <Integralist@users.noreply.github.com>
2024-04-18 10:01:55 +0100

    fix(kvstore): delete all keys (#1181)
    * fix(kvstore): delete all keys
    
    * refactor(kvstore): simplify logic
    
    * build: bump go-fastly
    
    * fix(kvstore): test
    
    * fix(kvstore): avoid memory exhaustion
    
    * fix(kvstore): allow user to set the max errors
    
    * doc(kvstoreentry/delete): comment magic number
    
    * fix(kvstore): add warning prompt
    
    * fix(kvstore): show deletion count
    
    * test(kvstore): clean up unused args

--- pkg/commands/kvstore/delete.go
+++ pkg/commands/kvstore/delete.go
@@ -1,11 +1,14 @@
 package kvstore
 
 import (
+	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/fastly/go-fastly/v9/fastly"
 
 	"github.com/fastly/cli/pkg/argparser"
+	"github.com/fastly/cli/pkg/commands/kvstoreentry"
 	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/global"
 	"github.com/fastly/cli/pkg/text"
@@ -16,7 +19,10 @@ type DeleteCommand struct {
 	argparser.Base
 	argparser.JSONOutput
 
-	Input fastly.DeleteKVStoreInput
+	deleteAll bool
+	maxErrors int
+	poolSize  int
+	Input     fastly.DeleteKVStoreInput
 }
 
 // NewDeleteCommand returns a usable command registered under the parent.
@@ -32,20 +38,50 @@ func NewDeleteCommand(parent argparser.Registerer, g *global.Data) *DeleteComman
 	c.CmdClause.Flag("store-id", "Store ID").Short('s').Required().StringVar(&c.Input.StoreID)
 
 	// Optional.
+	c.CmdClause.Flag("all", "Delete all entries within the store").Short('a').BoolVar(&c.deleteAll)
+	c.CmdClause.Flag("concurrency", "The thread pool size (ignored when set without the --all flag)").Default(strconv.Itoa(kvstoreentry.DeleteKeysPoolSize)).Short('r').IntVar(&c.poolSize)
 	c.RegisterFlagBool(c.JSONFlag()) // --json
+	c.CmdClause.Flag("max-errors", "The number of errors to accept before stopping (ignored when set without the --all flag)").Default(strconv.Itoa(kvstoreentry.DeleteKeysMaxErrors)).Short('m').IntVar(&c.maxErrors)
 	return &c
 }
 
 // Exec invokes the application logic for the command.
-func (c *DeleteCommand) Exec(_ io.Reader, out io.Writer) error {
+func (c *DeleteCommand) Exec(in io.Reader, out io.Writer) error {
 	if c.Globals.Verbose() && c.JSONOutput.Enabled {
 		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
+	if c.deleteAll {
+		if !c.Globals.Flags.AutoYes && !c.Globals.Flags.NonInteractive {
+			text.Warning(out, "This will delete ALL entries from your store!\n\n")
+			cont, err := text.AskYesNo(out, "Are you sure you want to continue? [y/N]: ", in)
+			if err != nil {
+				return err
+			}
+			if !cont {
+				return nil
+			}
+			text.Break(out)
+		}
+		dc := kvstoreentry.DeleteCommand{
+			Base: argparser.Base{
+				Globals: c.Globals,
+			},
+			DeleteAll: c.deleteAll,
+			MaxErrors: c.maxErrors,
+			PoolSize:  c.poolSize,
+			StoreID:   c.Input.StoreID,
+		}
+		if err := dc.DeleteAllKeys(out); err != nil {
+			return err
+		}
+		text.Break(out)
 	}
 
 	err := c.Globals.APIClient.DeleteKVStore(&c.Input)
 	if err != nil {
 		c.Globals.ErrLog.Add(err)
-		return err
+		return fmt.Errorf("failed to delete KV store: %w", err)
 	}
 
 	if c.JSONOutput.Enabled {
--- pkg/commands/kvstoreentry/delete.go
+++ pkg/commands/kvstoreentry/delete.go
@@ -3,9 +3,9 @@ package kvstoreentry
 import (
 	"fmt"
 	"io"
-	"sort"
-	"strings"
+	"strconv"
 	"sync"
+	"sync/atomic"
 
 	"github.com/fastly/go-fastly/v9/fastly"
 
@@ -15,19 +15,25 @@ import (
 	"github.com/fastly/cli/pkg/text"
 )
 
-// deleteKeysConcurrencyLimit is used to limit the concurrency when deleting ALL keys.
-// This is effectively the 'thread pool' size.
-const deleteKeysConcurrencyLimit int = 100
+// DeleteKeysPoolSize is the goroutine/thread-pool size.
+// Each pool will take a 'key' from a channel and issue a DELETE request.
+const DeleteKeysPoolSize int = 100
+
+// DeleteKeysMaxErrors is the maximum number of errors we'll allow before
+// stopping the goroutines from executing.
+const DeleteKeysMaxErrors int = 100
 
 // DeleteCommand calls the Fastly API to delete an kv store.
 type DeleteCommand struct {
 	argparser.Base
 	argparser.JSONOutput
+	key argparser.OptionalString
 
-	concurrency argparser.OptionalInt
-	deleteAll   bool
-	key         argparser.OptionalString
-	storeID     string
+	// NOTE: Public fields can be set via `kv-store delete`.
+	DeleteAll bool
+	MaxErrors int
+	PoolSize  int
+	StoreID   string
 }
 
 // NewDeleteCommand returns a usable command registered under the parent.
@@ -40,13 +46,14 @@ func NewDeleteCommand(parent argparser.Registerer, g *global.Data) *DeleteComman
 	c.CmdClause = parent.Command("delete", "Delete a key")
 
 	// Required.
-	c.CmdClause.Flag("store-id", "Store ID").Short('s').Required().StringVar(&c.storeID)
+	c.CmdClause.Flag("store-id", "Store ID").Short('s').Required().StringVar(&c.StoreID)
 
 	// Optional.
-	c.CmdClause.Flag("all", "Delete all entries within the store").Short('a').BoolVar(&c.deleteAll)
-	c.CmdClause.Flag("concurrency", "Control thread pool size (ignored when set without the --all flag)").Short('c').Action(c.concurrency.Set).IntVar(&c.concurrency.Value)
+	c.CmdClause.Flag("all", "Delete all entries within the store").Short('a').BoolVar(&c.DeleteAll)
+	c.CmdClause.Flag("concurrency", "The thread pool size (ignored when set without the --all flag)").Default(strconv.Itoa(DeleteKeysPoolSize)).Short('r').IntVar(&c.PoolSize)
 	c.RegisterFlagBool(c.JSONFlag()) // --json
 	c.CmdClause.Flag("key", "Key name").Short('k').Action(c.key.Set).StringVar(&c.key.Value)
+	c.CmdClause.Flag("max-errors", "The number of errors to accept before stopping (ignored when set without the --all flag)").Default(strconv.Itoa(DeleteKeysMaxErrors)).Short('m').IntVar(&c.MaxErrors)
 
 	return &c
 }
@@ -57,17 +64,17 @@ func (c *DeleteCommand) Exec(in io.Reader, out io.Writer) error {
 		return fsterr.ErrInvalidVerboseJSONCombo
 	}
 	// TODO: Support --json for bulk deletions.
-	if c.deleteAll && c.JSONOutput.Enabled {
+	if c.DeleteAll && c.JSONOutput.Enabled {
 		return fsterr.ErrInvalidDeleteAllJSONKeyCombo
 	}
-	if c.deleteAll && c.key.WasSet {
+	if c.DeleteAll && c.key.WasSet {
 		return fsterr.ErrInvalidDeleteAllKeyCombo
 	}
-	if !c.deleteAll && !c.key.WasSet {
+	if !c.DeleteAll && !c.key.WasSet {
 		return fsterr.ErrMissingDeleteAllKeyCombo
 	}
 
-	if c.deleteAll {
+	if c.DeleteAll {
 		if !c.Globals.Flags.AutoYes && !c.Globals.Flags.NonInteractive {
 			text.Warning(out, "This will delete ALL entries from your store!\n\n")
 			cont, err := text.AskYesNo(out, "Are you sure you want to continue? [y/N]: ", in)
@@ -79,11 +86,11 @@ func (c *DeleteCommand) Exec(in io.Reader, out io.Writer) error {
 			}
 			text.Break(out)
 		}
-		return c.deleteAllKeys(out)
+		return c.DeleteAllKeys(out)
 	}
 
 	input := fastly.DeleteKVStoreKeyInput{
-		StoreID: c.storeID,
+		StoreID: c.StoreID,
 		Key:     c.key.Value,
 	}
 
@@ -100,70 +107,107 @@ func (c *DeleteCommand) Exec(in io.Reader, out io.Writer) error {
 			Deleted bool   `json:"deleted"`
 		}{
 			c.key.Value,
-			c.storeID,
+			c.StoreID,
 			true,
 		}
 		_, err := c.WriteJSON(out, o)
 		return err
 	}
 
-	text.Success(out, "Deleted key '%s' from KV Store '%s'", c.key.Value, c.storeID)
+	text.Success(out, "Deleted key '%s' from KV Store '%s'", c.key.Value, c.StoreID)
 	return nil
 }
 
-func (c *DeleteCommand) deleteAllKeys(out io.Writer) error {
+// DeleteAllKeys deletes all keys within the specified KV Store.
+// NOTE: It's a public method as it can be called via `kv-store delete --all`.
+func (c *DeleteCommand) DeleteAllKeys(out io.Writer) error {
+	spinnerMessage := "Deleting keys"
+	var spinner text.Spinner
+
+	var err error
+	spinner, err = text.NewSpinner(out)
+	if err != nil {
+		return err
+	}
+	err = spinner.Start()
+	if err != nil {
+		return err
+	}
+	spinner.Message(spinnerMessage + "...")
+
 	p := c.Globals.APIClient.NewListKVStoreKeysPaginator(&fastly.ListKVStoreKeysInput{
-		StoreID: c.storeID,
+		StoreID: c.StoreID,
 	})
 
+	errorsCh := make(chan string, c.MaxErrors)
+	keysCh := make(chan string, 1000) // number correlates to pagination page size
+
 	var (
-		mu sync.Mutex
-		wg sync.WaitGroup
+		deleteCount atomic.Uint64
+		failedKeys  []string
+		wg          sync.WaitGroup
 	)
-	poolSize := deleteKeysConcurrencyLimit
-	if c.concurrency.WasSet {
-		poolSize = c.concurrency.Value
-	}
-	semaphore := make(chan struct{}, poolSize)
 
-	failedKeys := []string{}
+	// We have two separate execution flows happening at once:
+	//
+	// 1. Pushing keys from pagination data into a key channel.
+	// 2. Pulling keys from key channel and issuing API DELETE call.
+	//
+	// We have a limit on the number of errors. Once that limit is reached we'll
+	// stop the 2. set of goroutines.
 
-	for p.Next() {
-		// IMPORTANT: Use copies of the keys when processing data concurrently.
-		keys := p.Keys()
-		copiedKeys := make([]string, len(keys))
-		copy(copiedKeys, keys)
-
-		wg.Add(1)
-		go func(keys []string) {
-			semaphore <- struct{}{}
-			defer func() { <-semaphore }()
-			defer wg.Done()
-
-			sort.Strings(keys)
-			for _, key := range keys {
-				text.Output(out, "Deleting key: %s", key)
-				err := c.Globals.APIClient.DeleteKVStoreKey(&fastly.DeleteKVStoreKeyInput{StoreID: c.storeID, Key: key})
-				if err != nil {
-					c.Globals.ErrLog.Add(fmt.Errorf("failed to delete key '%s': %s", key, err))
-					mu.Lock()
-					failedKeys = append(failedKeys, key)
-					mu.Unlock()
-				}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(keysCh)
+		for p.Next() {
+			for _, key := range p.Keys() {
+				keysCh <- key
 			}
-		}(keys)
+		}
+	}()
+
+	for range c.PoolSize {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for key := range keysCh {
+				err := c.Globals.APIClient.DeleteKVStoreKey(&fastly.DeleteKVStoreKeyInput{StoreID: c.StoreID, Key: key})
+				if err != nil {
+					select {
+					case errorsCh <- key:
+					default:
+						return // channel is blocked
+					}
+				}
+				spinner.Message(spinnerMessage + "..." + strconv.FormatUint(deleteCount.Add(1), 10))
+			}
+		}()
 	}
 
 	wg.Wait()
-	close(semaphore)
 
-	if err := p.Err(); err != nil {
-		return fmt.Errorf("failed to delete keys: %s", err)
+	close(errorsCh)
+	for err := range errorsCh {
+		failedKeys = append(failedKeys, err)
 	}
+
+	spinnerMessage = "Deleted keys: " + strconv.FormatUint(deleteCount.Load(), 10)
+
 	if len(failedKeys) > 0 {
-		return fmt.Errorf("failed to delete keys: %s", strings.Join(failedKeys, ", "))
+		spinner.StopFailMessage(spinnerMessage)
+		err := spinner.StopFail()
+		if err != nil {
+			return fmt.Errorf("failed to stop spinner: %w", err)
+		}
+		return fmt.Errorf("failed to delete %d keys", len(failedKeys))
 	}
 
-	text.Success(out, "\nDeleted all keys from KV Store '%s'", c.storeID)
+	spinner.StopMessage(spinnerMessage)
+	if err := spinner.Stop(); err != nil {
+		return fmt.Errorf("failed to stop spinner: %w", err)
+	}
+
+	text.Success(out, "\nDeleted all keys from KV Store '%s'", c.StoreID)
 	return nil
 }
