47f71e9881d2170eb3a08da622a6728a2fe88951
Patrick Lindsay <plindsay@fastly.com>
2024-12-04 13:08:06 -0600

    Implement dashboard delete command

--- pkg/commands/dashboard/delete.go
+++ pkg/commands/dashboard/delete.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/fastly/cli/pkg/argparser"
 	"github.com/fastly/cli/pkg/global"
+	"github.com/fastly/cli/pkg/text"
 )
 
 // NewDeleteCommand returns a usable command registered under the parent.
@@ -15,8 +16,8 @@ func NewDeleteCommand(parent argparser.Registerer, globals *global.Data) *Delete
 	c.CmdClause = parent.Command("delete", "Delete a custom dashboard").Alias("remove")
 	c.Globals = globals
 
-	// Optional flags
-	c.RegisterFlagBool(c.JSONFlag()) // --json
+	// Required flags
+	c.CmdClause.Flag("id", "Alphanumeric string identifying a Dashboard").Required().StringVar(&c.dashboardID)
 
 	return &c
 }
@@ -24,17 +25,27 @@ func NewDeleteCommand(parent argparser.Registerer, globals *global.Data) *Delete
 // DeleteCommand calls the Fastly API to delete an appropriate resource.
 type DeleteCommand struct {
 	argparser.Base
-	argparser.JSONOutput
+
+	dashboardID string
 }
 
 // Exec invokes the application logic for the command.
 func (c *DeleteCommand) Exec(in io.Reader, out io.Writer) error {
+	input := c.constructInput()
+	err := c.Globals.APIClient.DeleteObservabilityCustomDashboard(input)
+	if err != nil {
+		return err
+	}
+
+	text.Success(out, "Deleted custom dashboard '%s'", fastly.ToValue(input.ID))
 	return nil
 }
 
 // constructInput transforms values parsed from CLI flags into an object to be used by the API client library.
-func (c *DeleteCommand) constructInput(serviceID string, serviceVersion int) *fastly.DeleteObservabilityCustomDashboardInput {
+func (c *DeleteCommand) constructInput() *fastly.DeleteObservabilityCustomDashboardInput {
 	var input fastly.DeleteObservabilityCustomDashboardInput
+
+	input.ID = &c.dashboardID
 
 	return &input
 }
