c4ffd9896533ab2ca1aefd7b07a2a6aba4a41b85
Richard Carillo <rcarillo@fastly.com>
2025-08-14 10:28:33 -0400

    testing

---
+++ pkg/commands/imageoptimizerdefaults/doc.go
@@ -0,0 +1,2 @@
+// Package imageoptimizerdefaults contains commands to configure default settings for Fastly Image Optimizer requests.
+package imageoptimizerdefaults
---
+++ pkg/commands/imageoptimizerdefaults/get.go
@@ -0,0 +1,107 @@
+package imageoptimizerdefaults
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/fastly/go-fastly/v11/fastly"
+
+	"github.com/fastly/cli/pkg/argparser"
+	fsterr "github.com/fastly/cli/pkg/errors"
+	"github.com/fastly/cli/pkg/global"
+)
+
+// GetCommand calls the Fastly API to describe the Image Optimizer default settings for a service.
+type GetCommand struct {
+	argparser.Base
+	argparser.JSONOutput
+
+	Input          fastly.GetImageOptimizerDefaultSettingsInput
+	serviceName    argparser.OptionalServiceNameID
+	serviceVersion argparser.OptionalServiceVersion
+}
+
+// NewGetCommand returns a usable command registered under the parent.
+func NewGetCommand(parent argparser.Registerer, g *global.Data) *GetCommand {
+	c := GetCommand{
+		Base: argparser.Base{
+			Globals: g,
+		},
+	}
+	c.CmdClause = parent.Command("get", "Retrieve the current Image Optimizer default settings")
+
+	// Required.
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Name:        argparser.FlagServiceIDName,
+		Description: argparser.FlagServiceIDDesc,
+		Dst:         &g.Manifest.Flag.ServiceID,
+		Short:       's',
+		Required:    true,
+	})
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Name:        argparser.FlagVersionName,
+		Description: argparser.FlagVersionDesc,
+		Dst:         &c.serviceVersion.Value,
+		Required:    true,
+	})
+
+	// Optional.
+	c.RegisterFlagBool(c.JSONFlag()) // --json
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Action:      c.serviceName.Set,
+		Name:        argparser.FlagServiceName,
+		Description: argparser.FlagServiceNameDesc,
+		Dst:         &c.serviceName.Value,
+	})
+	return &c
+}
+
+// Exec invokes the application logic for the command.
+func (c *GetCommand) Exec(_ io.Reader, out io.Writer) error {
+	if c.Globals.Verbose() && c.JSONOutput.Enabled {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
+	serviceID, serviceVersion, err := argparser.ServiceDetails(argparser.ServiceDetailsOpts{
+		APIClient:          c.Globals.APIClient,
+		Manifest:           *c.Globals.Manifest,
+		Out:                out,
+		ServiceNameFlag:    c.serviceName,
+		ServiceVersionFlag: c.serviceVersion,
+		VerboseMode:        c.Globals.Flags.Verbose,
+	})
+	if err != nil {
+		c.Globals.ErrLog.AddWithContext(err, map[string]any{
+			"Service ID":      serviceID,
+			"Service Version": fsterr.ServiceVersion(serviceVersion),
+		})
+		return err
+	}
+
+	c.Input.ServiceID = serviceID
+	c.Input.ServiceVersion = fastly.ToValue(serviceVersion.Number)
+
+	o, err := c.Globals.APIClient.GetImageOptimizerDefaultSettings(context.TODO(), &c.Input)
+	if err != nil {
+		c.Globals.ErrLog.AddWithContext(err, map[string]any{
+			"Service ID":      serviceID,
+			"Service Version": fastly.ToValue(serviceVersion.Number),
+		})
+		return err
+	}
+
+	if ok, err := c.WriteJSON(out, o); ok {
+		return err
+	}
+
+	fmt.Fprintf(out, "Allow Video: %t\n", o.AllowVideo)
+	fmt.Fprintf(out, "JPEG Quality: %d\n", o.JpegQuality)
+	fmt.Fprintf(out, "JPEG Type: %s\n", o.JpegType)
+	fmt.Fprintf(out, "Resize Filter: %s\n", o.ResizeFilter)
+	fmt.Fprintf(out, "Upscale: %t\n", o.Upscale)
+	fmt.Fprintf(out, "WebP: %t\n", o.Webp)
+	fmt.Fprintf(out, "WebP Quality: %d\n", o.WebpQuality)
+
+	return nil
+}
