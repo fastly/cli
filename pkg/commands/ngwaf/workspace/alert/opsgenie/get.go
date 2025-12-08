0e669d1f9d40c2c73bd1baae713fcc108dc2449f
Richard Carillo <77027245+rcaril@users.noreply.github.com>
2025-12-08 13:05:12 -0500

    [CDTOOL- 1216] Support for NGWAF Alerts (#1589)
    ### Change summary
    
     <!--
    Briefly describe the changes introduced in this pull request. Include
    context or
    reasoning behind the changes, even if they seem minor. If relevant, link
    to any
    related discussions (e.g. Slack threads, tickets, documents).
    -->
    
     All Submissions:
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull
    Requests](https://github.com/fastly/cli/pulls) for the same
    update/change?
    
    <!-- You can erase any parts of this template not applicable to your
    Pull Request. -->
    
    ### New Feature Submissions:
    
    * [x] Does your submission pass tests?
    
    ### Changes to Core Features:
    
    * [x] Have you written new tests for your core changes, as applicable?
    * [x] Have you successfully run tests with your changes locally?

---
+++ pkg/commands/ngwaf/workspace/alert/opsgenie/get.go
@@ -0,0 +1,86 @@
+package opsgenie
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"github.com/fastly/cli/pkg/argparser"
+	fsterr "github.com/fastly/cli/pkg/errors"
+	"github.com/fastly/cli/pkg/global"
+	"github.com/fastly/cli/pkg/text"
+	"github.com/fastly/go-fastly/v12/fastly"
+	"github.com/fastly/go-fastly/v12/fastly/ngwaf/v1/workspaces/alerts/opsgenie"
+)
+
+// GetCommand calls the Fastly API to get Opsgenie alerts.
+type GetCommand struct {
+	argparser.Base
+	argparser.JSONOutput
+
+	// Required.
+	AlertID     string
+	WorkspaceID argparser.OptionalWorkspaceID
+}
+
+// NewGetCommand returns a usable command registered under the parent.
+func NewGetCommand(parent argparser.Registerer, g *global.Data) *GetCommand {
+	c := GetCommand{
+		Base: argparser.Base{
+			Globals: g,
+		},
+	}
+	c.CmdClause = parent.Command("get", "Get a Opsgenie alert")
+
+	// Required.
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Name:        argparser.FlagNGWAFWorkspaceID,
+		Description: argparser.FlagNGWAFWorkspaceIDDesc,
+		Dst:         &c.WorkspaceID.Value,
+		Action:      c.WorkspaceID.Set,
+	})
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Name:        argparser.FlagNGWAFAlertID,
+		Description: argparser.FlagNGWAFAlertIDDesc,
+		Dst:         &c.AlertID,
+		Required:    true,
+	})
+
+	// Optional.
+	c.RegisterFlagBool(c.JSONFlag())
+
+	return &c
+}
+
+// Exec invokes the application logic for the command.
+func (c *GetCommand) Exec(_ io.Reader, out io.Writer) error {
+	// Call Parse() to ensure that we check if workspaceID
+	// is set or to throw the appropriate error.
+	if err := c.WorkspaceID.Parse(); err != nil {
+		return err
+	}
+	if c.Globals.Verbose() && c.JSONOutput.Enabled {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+	input := &opsgenie.GetInput{
+		AlertID:     &c.AlertID,
+		WorkspaceID: &c.WorkspaceID.Value,
+	}
+
+	fc, ok := c.Globals.APIClient.(*fastly.Client)
+	if !ok {
+		return errors.New("failed to convert interface to a fastly client")
+	}
+
+	data, err := opsgenie.Get(context.TODO(), fc, input)
+	if err != nil {
+		return err
+	}
+
+	if ok, err := c.WriteJSON(out, data); ok {
+		return err
+	}
+
+	text.PrintAlert(out, data)
+	return nil
+}
---
+++ pkg/commands/ngwaf/workspace/alert/slack/doc.go
@@ -0,0 +1,2 @@
+// Package slack contains commands to inspect and manipulate NGWAF Slack alerts.
+package slack
