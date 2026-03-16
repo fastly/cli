50b937db60bfcc7ab10b26089f90c52276ad9080
Anthony Gomez <anthony.gomez@fastly.com>
2026-03-16 16:25:08 -0400

    Operations tags crud commands (#1688)
    ### Change summary
    Adds crud operations for api security operations tags
    
     All Submissions:
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull
    Requests](https://github.com/fastly/cli/pulls) for the same
    update/change?
    
    <!-- You can erase any parts of this template not applicable to your
    Pull Request. -->
    
    ### New Feature Submissions:
    
    * [x] Does your submission pass tests?
    
    ### Changes to Core Features:
    
    * [x] Have you written new tests for your core changes, as applicable?
    * [x] Have you successfully run tests with your changes locally?
    
    ---------
    
    Co-authored-by: Richard Carillo <77027245+rcaril@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@
 ### Breaking:
 
 ### Bug Fixes:
-- fix(stats): `stats historical` now returns write errors instead of silently swallowing them. [#1678](https://github.com/fastly/cli/pull/1678)
+- fix(stats): `stats historical` now returns write errors instead of silently swallowing them [#1678](https://github.com/fastly/cli/pull/1678)
 
 ### Enhancements:
 - feat(stats): add `--field` flag to `stats historical` to filter to a single stats field. [#1678](https://github.com/fastly/cli/pull/1678)
@@ -15,6 +15,7 @@
 - feat(stats): add `stats origin-inspector` subcommand for origin-level metrics. [#1678](https://github.com/fastly/cli/pull/1678)
 - feat(apisecurity/discoveredoperations): add support for 'list' and 'update' support for 'API discovery'. [#1689](https://github.com/fastly/cli/pull/1689)
 - feat(apisecurity/operations): add CRUD support for 'API Inventory' operations. [#1689](https://github.com/fastly/cli/pull/1689)
+- feat(apisecurity/tags): add API Security Operations tag support ([#1688](https://github.com/fastly/cli/pull/1688))
 
 ### Dependencies:
 - build(deps): `golang.org/x/net` from 0.50.0 to 0.51.0 ([#1674](https://github.com/fastly/cli/pull/1674))
---
+++ pkg/commands/apisecurity/tags/get.go
@@ -0,0 +1,95 @@
+package tags
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"github.com/fastly/go-fastly/v13/fastly"
+	"github.com/fastly/go-fastly/v13/fastly/apisecurity/operations"
+
+	"github.com/fastly/cli/pkg/argparser"
+	fsterr "github.com/fastly/cli/pkg/errors"
+	"github.com/fastly/cli/pkg/global"
+	"github.com/fastly/cli/pkg/text"
+)
+
+// GetCommand calls the Fastly API to get an operation tag.
+type GetCommand struct {
+	argparser.Base
+	argparser.JSONOutput
+
+	// Required.
+	tagID       string
+	serviceName argparser.OptionalServiceNameID
+}
+
+// NewGetCommand returns a usable command registered under the parent.
+func NewGetCommand(parent argparser.Registerer, g *global.Data) *GetCommand {
+	c := GetCommand{
+		Base: argparser.Base{
+			Globals: g,
+		},
+	}
+
+	c.CmdClause = parent.Command("get", "Get an operation tag")
+
+	// Required.
+	c.CmdClause.Flag("tag-id", "Tag ID").Required().StringVar(&c.tagID)
+
+	// Optional.
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Name:        argparser.FlagServiceIDName,
+		Description: argparser.FlagServiceIDDesc,
+		Dst:         &g.Manifest.Flag.ServiceID,
+	})
+	c.RegisterFlag(argparser.StringFlagOpts{
+		Action:      c.serviceName.Set,
+		Name:        argparser.FlagServiceName,
+		Description: argparser.FlagServiceNameDesc,
+		Dst:         &c.serviceName.Value,
+	})
+	c.RegisterFlagBool(c.JSONFlag())
+
+	return &c
+}
+
+// Exec invokes the application logic for the command.
+func (c *GetCommand) Exec(_ io.Reader, out io.Writer) error {
+	if c.Globals.Verbose() && c.JSONOutput.Enabled {
+		return fsterr.ErrInvalidVerboseJSONCombo
+	}
+
+	serviceID, source, flag, err := argparser.ServiceID(c.serviceName, *c.Globals.Manifest, c.Globals.APIClient, c.Globals.ErrLog)
+	if err != nil {
+		return err
+	}
+	if c.Globals.Verbose() {
+		argparser.DisplayServiceID(serviceID, flag, source, out)
+	}
+
+	if serviceID == "" {
+		return errors.New("service-id is required")
+	}
+
+	fc, ok := c.Globals.APIClient.(*fastly.Client)
+	if !ok {
+		return errors.New("failed to convert interface to a fastly client")
+	}
+
+	tag, err := operations.DescribeTag(context.TODO(), fc, &operations.DescribeTagInput{
+		ServiceID: &serviceID,
+		TagID:     &c.tagID,
+	})
+	if err != nil {
+		c.Globals.ErrLog.Add(err)
+		return err
+	}
+
+	if ok, err := c.WriteJSON(out, tag); ok {
+		return err
+	}
+
+	text.PrintOperationTag(out, tag)
+	return nil
+}
