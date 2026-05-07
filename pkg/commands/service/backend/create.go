c7f333731574551e1a082d6b4554f3b773123358
Richard Carillo <77027245+rcaril@users.noreply.github.com>
2026-05-07 10:44:49 -0400

    [CDTOOL-1317] Add Support for the `max_use` and `max_lifetime` backend parameters (#1779)
    ### Change summary
    
    This PR adds new `--max-use` and `--max-lifetime` parameters to `fastly
    service backend create` and `fastly service backend update`, which can
    be used to specify the `max_use` and `max_lifetime` backend fields.
    
     All Submissions:
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull
    Requests](https://github.com/fastly/cli/pulls) for the same
    update/change?
    
    <!-- You can erase any parts of this template not applicable to your
    Pull Request. -->
    
    ### New Feature Submissions:
    
    * [x] Does your submission pass tests?
    
    ```
    make test TEST_ARGS="-run TestBackendCreate ./pkg/commands/service/backend"
    ok      github.com/fastly/cli/pkg/commands/service/backend      1.204s
    
    make test TEST_ARGS="-run TestBackendList ./pkg/commands/service/backend"
    ok      github.com/fastly/cli/pkg/commands/service/backend      1.130s
    
    make test TEST_ARGS="-run TestBackendDescribe ./pkg/commands/service/backend"
    ok      github.com/fastly/cli/pkg/commands/service/backend      1.071s
    
    make test TEST_ARGS="-run TestBackendUpdate ./pkg/commands/service/backend"
    ok      github.com/fastly/cli/pkg/commands/service/backend      1.133s
    
    make test TEST_ARGS="-run TestBackendDelete ./pkg/commands/service/backend"
    ok      github.com/fastly/cli/pkg/commands/service/backend      1.065s
    ```
    
    ### Changes to Core Features:
    
    * [x] Have you written new tests for your core changes, as applicable?
    * [x] Have you successfully run tests with your changes locally?

--- pkg/commands/service/backend/create.go
+++ pkg/commands/service/backend/create.go
@@ -32,6 +32,8 @@ type CreateCommand struct {
 	firstByteTimeout    argparser.OptionalInt
 	healthCheck         argparser.OptionalString
 	maxConn             argparser.OptionalInt
+	maxUse              argparser.OptionalInt
+	maxLifetime         argparser.OptionalInt
 	maxTLSVersion       argparser.OptionalString
 	minTLSVersion       argparser.OptionalString
 	name                argparser.OptionalString
@@ -89,6 +91,8 @@ func NewCreateCommand(parent argparser.Registerer, g *global.Data) *CreateComman
 	c.CmdClause.Flag("first-byte-timeout", "How long to wait for the first bytes in milliseconds").Action(c.firstByteTimeout.Set).IntVar(&c.firstByteTimeout.Value)
 	c.CmdClause.Flag("healthcheck", "The name of the healthcheck to use with this backend").Action(c.healthCheck.Set).StringVar(&c.healthCheck.Value)
 	c.CmdClause.Flag("max-conn", "Maximum number of connections").Action(c.maxConn.Set).IntVar(&c.maxConn.Value)
+	c.CmdClause.Flag("max-use", "Maximum number of requests allowed over a single, pooled HTTP keepalive connection to this backend; 0 is treated as unlimited.").Action(c.maxUse.Set).IntVar(&c.maxUse.Value)
+	c.CmdClause.Flag("max-lifetime", "Maximum time from creation (in milliseconds) that a pooled HTTP keepalive connection will be eligible for reuse; 0 is treated as unlimited.").Action(c.maxLifetime.Set).IntVar(&c.maxLifetime.Value)
 	c.CmdClause.Flag("max-tls-version", "Maximum allowed TLS version on SSL connections to this backend").Action(c.maxTLSVersion.Set).StringVar(&c.maxTLSVersion.Value)
 	c.CmdClause.Flag("min-tls-version", "Minimum allowed TLS version on SSL connections to this backend").Action(c.minTLSVersion.Set).StringVar(&c.minTLSVersion.Value)
 	c.CmdClause.Flag("name", "Backend name").Short('n').Action(c.name.Set).StringVar(&c.name.Value)
@@ -179,6 +183,12 @@ func (c *CreateCommand) Exec(_ io.Reader, out io.Writer) error {
 	}
 	if c.maxConn.WasSet {
 		input.MaxConn = &c.maxConn.Value
+	}
+	if c.maxUse.WasSet {
+		input.MaxUse = &c.maxUse.Value
+	}
+	if c.maxLifetime.WasSet {
+		input.MaxLifetime = &c.maxLifetime.Value
 	}
 	if c.maxTLSVersion.WasSet {
 		input.MaxTLSVersion = &c.maxTLSVersion.Value
